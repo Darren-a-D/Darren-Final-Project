bc37cc3376345940ae233909ecd7b225b088fefd
Darren-a-D <darrenkl@hawaii.edu>
2025-05-07 22:48:35 -1000

    !
    AHh

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+    Student Name: Darren Louis
+    File Name: script.js
+    Date: 05/07/2025
+*/
+
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Have a wonderful day";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
